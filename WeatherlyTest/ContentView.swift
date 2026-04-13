ea29636d6214a0a078a065817fdc05906c38b789
ShreyasTantri <shreyastantrik29@gmail.com>
2026-04-13 18:27:23 +0530

    Initial Commit

---
+++ WeatherlyTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherlyTest
+//
+//  Created by CCS038 on 13/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherlyTest/WeatherlyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyTestApp.swift
+//  WeatherlyTest
+//
+//  Created by CCS038 on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
